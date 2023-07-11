8443352d364cf065abe64e62108702f4d9750136
ulisem <ulises@parts-badger.com>
2023-07-11 16:49:36 -0600

    Update HomeScreen.tsx

--- components/Screens/HomeScreen.tsx
+++ components/Screens/HomeScreen.tsx
@@ -5,10 +5,10 @@ export default function HomeScreen() {
     return (
     <>
             <div className="video-background">
-                    <iframe src="https://www.youtube.com/embed/Y8m9UjjD0Zc?autoplay=1&mute=1&controls=0&showinfo=0&fs=0&vq=hd1080&nablejsapi=1&loop=1&playlist=Y8m9UjjD0Zc" width={'100%'} height='700px' 
+                    <iframe src="https://www.youtube.com/embed/Y8m9UjjD0Zc?autoplay=1&mute=1&controls=0&showinfo=0&fs=0&vq=hd1080&nablejsapi=1&loop=1&playlist=Y8m9UjjD0Zc" width={'100%'} height='500px' 
           frameBorder='0'></iframe>
                 <TypeAnimation
-                style={{position:'absolute',left:'25%',top:'20%',fontSize:'2rem',fontFamily:'futura'}}
+                style={{position:'absolute',left:'25%',top:'10%',fontSize:'2rem',fontFamily:'futura'}}
                     sequence={[
                         "ULISES MORA ", // Types 'One'
                         1000, // Waits 1s
